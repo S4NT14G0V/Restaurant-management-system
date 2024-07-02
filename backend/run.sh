ddec78f49c915495abb5a5fa27ffe815a79a8462
DavidGomezAgudelo <david.gomez.agudelo9@gmail.com>
2024-07-02 12:46:00 -0500

    class

---
+++ backend/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./mvnw.cmd spring-boot:run
---
+++ backend/src/main/java/com/backend/backend/model/EmployeeModel.java
@@ -0,0 +1,94 @@
+package com.backend.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name= "Employee")
+
+public class EmployeeModel {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(unique=true, nullable = false)
+    private int id;
+    private Long identfication;
+    private String name;
+    private Long phone;
+    private String job;
+    private Double salary;
+    private String horary; 
+
+    
+    public EmployeeModel(String horary, int id, Long identfication, String job, String name, Long phone, Double salary) {
+        this.horary = horary;
+        this.id = id;
+        this.identfication = identfication;
+        this.job = job;
+        this.name = name;
+        this.phone = phone;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Long getIdentfication() {
+        return identfication;
+    }
+
+    public void setIdentfication(Long identfication) {
+        this.identfication = identfication;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getHorary() {
+        return horary;
+    }
+
+    public void setHorary(String horary) {
+        this.horary = horary;
+    }
+
+
+    
+}
