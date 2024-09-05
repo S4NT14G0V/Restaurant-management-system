e7fdb96b41b5ed152b460f1c1e67cc9603abdf4f
DavidGomezAgudelo <david.gomez.agudelo9@gmail.com>
2024-09-05 12:57:39 -0500

    Update ApiUrls.js

--- frontend/src/pages/ApiUrls.js
+++ frontend/src/pages/ApiUrls.js
@@ -2,14 +2,14 @@ export const apiUrls = {
 
     employee: {
         'all': 'https://backendtest-5wti.onrender.com/employee/getAll',
-        "create": 'http://localhost:8080/employee/create'
+        "create": 'https://backendtest-5wti.onrender.com/employee/create'
     },
     provider:{
         'all': 'https://backendtest-5wti.onrender.com/provider/getAll',
-        "create": 'http://localhost:8080/provider/create'
+        "create": 'https://backendtest-5wti.onrender.com/create'
     },
     table:{
         'all': 'https://backendtest-5wti.onrender.com/table/getAll',
-        "create": 'http://localhost:8080/table/create'
+        "create": 'https://backendtest-5wti.onrender.com/table/create'
     }
 }
